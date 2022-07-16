73ef8207231ec9f920ec206c69c829ef0076ce41
Fireflyest <fireflyest@qq.com>
2022-07-16 23:26:55 +0800

    添加Lore和头颅设置

--- src/main/java/org/fireflyest/craftgui/util/ItemUtils.java
+++ src/main/java/org/fireflyest/craftgui/util/ItemUtils.java
@@ -1,7 +1,9 @@
 package org.fireflyest.craftgui.util;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +43,41 @@ public class ItemUtils {
             }
             lores.add(lore);
             meta.setLore(lores);
+            item.setItemMeta(meta);
+        }
+    }
+
+    /**
+     * 设置物品注释
+     * @param item 物品
+     * @param lore 注释
+     * @param line 行
+     */
+    public static void setLore(ItemStack item, String lore, int line){
+        ItemMeta meta = item.getItemMeta();
+        if(meta != null){
+            List<String> lores = item.getItemMeta().getLore();
+            if (lores == null) {
+                lores = new ArrayList<>();
+            }
+            while (lores.size() <= line){
+                lores.add("");
+            }
+            lores.set(line, lore);
+            meta.setLore(lores);
+            item.setItemMeta(meta);
+        }
+    }
+
+    /**
+     * 设置玩家头颅
+     * @param item 物品
+     * @param player 头颅
+     */
+    public static void setSkullOwner(ItemStack item, OfflinePlayer player){
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        if (meta != null) {
+            meta.setOwningPlayer(player);
             item.setItemMeta(meta);
         }
     }
