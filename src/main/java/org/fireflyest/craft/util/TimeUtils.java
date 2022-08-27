69b80d496dfdfab4502e5cff9b4ece11fea051c4
Fireflyest <fireflyest@qq.com>
2022-08-27 20:47:32 +0800

    时间工具类

--- src/main/java/org/fireflyest/craft/util/TimeUtils.java
+++ src/main/java/org/fireflyest/craft/util/TimeUtils.java
@@ -1,7 +1,9 @@
 package org.fireflyest.craft.util;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
 /**
  * @author Fireflyest
@@ -12,12 +14,46 @@ public class TimeUtils {
     private TimeUtils() {
     }
 
+    /**
+     * 获取当前纪元时间
+     * @return 毫秒
+     */
+    public static long getTime(){
+        return Instant.now().toEpochMilli();
+    }
+
+    /**
+     * 获取当前时间
+     * @return 时间字符串
+     */
     public static String getLocalTime(){
         return LocalTime.now().toString();
     }
 
+    /**
+     * 转化成时间字符串
+     * @param time 纪元时间
+     * @return 时间字符串
+     */
+    public static String getLocalTime(long time){
+        return LocalTime.ofInstant(Instant.ofEpochMilli(time), ZoneId.systemDefault()).toString();
+    }
+
+    /**
+     * 获取当前日期
+     * @return 日期字符串
+     */
     public static String getLocalDate(){
         return LocalDate.now().toString();
+    }
+
+    /**
+     * 转化成日期字符串
+     * @param time 纪元时间
+     * @return 日期字符串
+     */
+    public static String getLocalDate(long time){
+        return LocalDate.ofInstant(Instant.ofEpochMilli(time), ZoneId.systemDefault()).toString();
     }
 
 }
