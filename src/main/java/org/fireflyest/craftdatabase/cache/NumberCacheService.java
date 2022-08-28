a4a811c1e5a522835dc9b9f8009284efe1e21992
Fireflyest <fireflyest@qq.com>
2022-08-28 20:25:37 +0800

    添加缓存实现

---
+++ src/main/java/org/fireflyest/craftdatabase/cache/NumberCacheService.java
@@ -0,0 +1,42 @@
+package org.fireflyest.craftdatabase.cache;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Fireflyest
+ * @since 2022/8/28
+ */
+public class NumberCacheService extends CacheService<Number>{
+
+    public NumberCacheService() {
+    }
+
+    public void incr(@Nonnull String key){
+        incrBy(key, 1);
+    }
+
+    public void incrBy(@Nonnull String key, Number n){
+        Cache<Number> cache;
+        if ((cache = cacheMap.get(key)) != null) {
+            Number number = cache.get();
+            if (number != null) {
+                cache.set(number.doubleValue() + n.doubleValue());
+            }
+        }
+    }
+
+    public void decr(@Nonnull String key){
+        decrBy(key, 1);
+    }
+
+    public void decrBy(@Nonnull String key, Number n){
+        Cache<Number> cache;
+        if ((cache = cacheMap.get(key)) != null) {
+            Number number = cache.get();
+            if (number != null) {
+                cache.set(number.doubleValue() - n.doubleValue());
+            }
+        }
+    }
+
+}
