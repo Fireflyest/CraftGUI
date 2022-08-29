605af911490270c70f4e0f1ead18dd8d3e42bcfe
Fireflyest <fireflyest@qq.com>
2022-08-30 00:08:53 +0800

    按钮构建类

--- src/main/java/org/fireflyest/craftgui/button/ButtonItemBuilder.java
+++ src/main/java/org/fireflyest/craftgui/button/ButtonItemBuilder.java
@@ -29,4 +29,59 @@ public class ButtonItemBuilder extends ItemBuilder {
         return this;
     }
 
+    public ButtonItemBuilder actionPageNext(){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PAGE_NEXT);
+        return this;
+    }
+
+    public ButtonItemBuilder actionPagePre(){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PAGE_PRE);
+        return this;
+    }
+
+    public ButtonItemBuilder actionPageJump(int num){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PAGE_JUMP);
+        nbt.put(ButtonAction.NBT_VALUE_KEY, num);
+        return this;
+    }
+
+    public ButtonItemBuilder actionBack(){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_BACK);
+        return this;
+    }
+
+    public ButtonItemBuilder actionEdit(){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_EDIT);
+        return this;
+    }
+
+    public ButtonItemBuilder actionOpen(){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_OPEN);
+        return this;
+    }
+
+    public ButtonItemBuilder actionPlayerCommand(String command){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PLAYER_COMMAND);
+        nbt.put(ButtonAction.NBT_VALUE_KEY, command);
+        return this;
+    }
+
+    public ButtonItemBuilder actionConsoleCommand(String command){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_CONSOLE_COMMAND);
+        nbt.put(ButtonAction.NBT_VALUE_KEY, command);
+        return this;
+    }
+
+    public ButtonItemBuilder actionConsoleCommand(int page){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PAGE);
+        nbt.put(ButtonAction.NBT_VALUE_KEY, page);
+        return this;
+    }
+
+    public ButtonItemBuilder actionPlugin(String value){
+        nbt.put(ButtonAction.NBT_ACTION_KEY, ButtonAction.ACTION_PLUGIN);
+        nbt.put(ButtonAction.NBT_VALUE_KEY, value);
+        return this;
+    }
+
 }
