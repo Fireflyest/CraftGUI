6ab7d445ffd4bf2ab16bec4e03c520091135c384
Fireflyest <fireflyest@qq.com>
2022-09-17 18:22:15 +0800

    消除主类警告

--- src/main/java/org/fireflyest/CraftGUI.java
+++ src/main/java/org/fireflyest/CraftGUI.java
@@ -18,36 +18,26 @@ import org.fireflyest.craftgui.view.SimpleView;
 public final class CraftGUI extends JavaPlugin {
 
     private static JavaPlugin plugin;
-
-    public static JavaPlugin getPlugin(){
-        return plugin;
-    }
-
     private static ViewGuideImpl viewGuide;
 
-    public static ViewGuideImpl getViewGuide() {
-        return viewGuide;
-    }
-
     public static final String SIMPLE_VIEW = "craftgui.simple";
-
     public static final int BUKKIT_VERSION = XMaterial.getVersion();
 
     @Override
     public void onEnable() {
-        plugin = this;
+        CraftGUI.setPlugin(this);
 
         // 统计
         new Metrics(this, 15676);
+
         // 新建导航
-        viewGuide = new ViewGuideImpl();
-        viewGuide.addView(SIMPLE_VIEW, new SimpleView("[CraftGUI]"));
+        ViewGuideImpl viewGuideImpl = new ViewGuideImpl();
+        this.getServer().getServicesManager().register(ViewGuide.class, viewGuideImpl, plugin, ServicePriority.Normal);
+        viewGuideImpl.addView(SIMPLE_VIEW, new SimpleView("[CraftGUI]"));
+        CraftGUI.setViewGuide(viewGuideImpl);
 
         // 注册监听
-        this.getServer().getPluginManager().registerEvents( new ViewEventListener(), this);
-
-        // 注册服务
-        this.getServer().getServicesManager().register(ViewGuide.class, viewGuide, plugin, ServicePriority.Normal);
+        this.getServer().getPluginManager().registerEvents(new ViewEventListener(), this);
 
         // 初始化监听
         ViewProtocol.initViewProtocol();
@@ -61,4 +51,37 @@ public final class CraftGUI extends JavaPlugin {
         // 关闭所有数据库连接
         SQLConnector.closeAll();
     }
+
+    /**
+     * 设置其他类可获取的导航对象
+     * @param guide 导航
+     */
+    public static void setViewGuide(ViewGuideImpl guide) {
+        CraftGUI.viewGuide = guide;
+    }
+
+    /**
+     * 设置其他类可获取的插件对象
+     * @param javaPlugin 插件
+     */
+    public static void setPlugin(JavaPlugin javaPlugin) {
+        CraftGUI.plugin = javaPlugin;
+    }
+
+    /**
+     * 获取导航
+     * @return 导航
+     */
+    public static ViewGuideImpl getViewGuide() {
+        return viewGuide;
+    }
+
+    /**
+     * 获取插件
+     * @return 插件
+     */
+    public static JavaPlugin getPlugin() {
+        return plugin;
+    }
+
 }
