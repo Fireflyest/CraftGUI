f03c2f0199b52e0486c8a0c78babf5db4dc658cc
Fireflyest <fireflyest@qq.com>
2022-09-17 18:42:27 +0800

    更新页模板

--- src/main/java/org/fireflyest/craftgui/view/TemplatePage.java
+++ src/main/java/org/fireflyest/craftgui/view/TemplatePage.java
@@ -1,20 +1,28 @@
 package org.fireflyest.craftgui.view;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.fireflyest.craftgui.api.ViewPage;
 
+<<<<<<< HEAD
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+=======
+>>>>>>> 9d73041 (更新页模板)
 /**
  * 模板页面
  * @author Fireflyest
- * 2022/2/15 0:00
  */
 public abstract class TemplatePage implements ViewPage {
 
@@ -25,8 +33,6 @@ public abstract class TemplatePage implements ViewPage {
 
     // 界面载体
     protected Inventory inventory;
-    // 插件名称，用来判断界面由谁处理
-    protected final String pluginName;
     // 目标
     protected final String target;
     // 页码
@@ -37,35 +43,29 @@ public abstract class TemplatePage implements ViewPage {
     protected ViewPage next = null;
     protected ViewPage pre = null;
 
-    public TemplatePage(String pluginName, String target, int page, int size) {
-        this.pluginName = pluginName;
+    /**
+     * 新建一个页面
+     * @param title 标题
+     * @param target 归属
+     * @param page 页码
+     * @param size 大小
+     */
+    protected TemplatePage(String title, String target, int page, int size) {
         this.target = target;
         this.page = page;
         this.size = size;
 
-        // 标题
-        String guiTitle = pluginName;
-        if (target != null)  guiTitle += ("§9" + target);
-        if (page != -1) guiTitle += (" §7#§8" + page);
-
         // 界面容器
-        this.updateTitle(guiTitle);
+        this.updateTitle(title);
         // 界面固定按钮
         this.refreshPage();
     }
 
     @Nonnull
     @Override
-    public Inventory getInventory(){
+    public Inventory getInventory() {
         return inventory;
     }
-
-    /**
-     * 由异步线程调用
-     * @return 容器物品
-     */
-    @Override
-    public abstract @Nonnull Map<Integer, ItemStack> getItemMap();
 
     @Override
     public @Nonnull Map<Integer, ItemStack> getButtonMap() {
@@ -107,15 +107,8 @@ public abstract class TemplatePage implements ViewPage {
         this.next = next;
     }
 
-    /**
-     * 可能异步
-     */
-    @Override
-    public abstract void refreshPage();
-
     @Override
     public void updateTitle(String title) {
-        // 下次启动容器才能发现不同，如果需要实时更新，需要强制让玩家重新打开界面
         inventory = Bukkit.createInventory(null, size, title);
     }
 
