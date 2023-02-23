a2ef9e8d144c62032c17597422c825145ae5e0e8
Fireflyest <fireflyest@qq.com>
2023-02-23 14:58:11 +0800

    优化指令提示

--- src/main/java/org/fireflyest/craftcommand/argument/BooleanArgs.java
+++ src/main/java/org/fireflyest/craftcommand/argument/BooleanArgs.java
@@ -3,10 +3,14 @@ package org.fireflyest.craftcommand.argument;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
+import org.bukkit.command.CommandSender;
+
 public class BooleanArgs implements Argument {
 
     @Override
-    public List<String> tab(String arg) {
+    public List<String> tab(@Nonnull CommandSender sender, @Nonnull String arg) {
         List<String> argList = new ArrayList<>();
         if (arg.startsWith("true")) {
             argList.add("true");
