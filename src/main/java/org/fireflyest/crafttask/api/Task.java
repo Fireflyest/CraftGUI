40d9aacc365489e5a32e786f0cef3ddd8e5079de
Fireflyest <fireflyest@qq.com>
2023-06-04 16:56:41 +0800

    完善任务

--- src/main/java/org/fireflyest/crafttask/api/Task.java
+++ src/main/java/org/fireflyest/crafttask/api/Task.java
@@ -5,10 +5,20 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.fireflyest.crafttask.exception.ExecuteException;
 
 public abstract class Task {
     
+    private final String playerName;
+    private final Player player;
+
+    protected Task(String playerName) {
+        this.playerName = playerName;
+        this.player = Bukkit.getPlayerExact(playerName);
+    }
+
     // 产生的新任务
     protected final List<Task> follows = new ArrayList<>();
 
@@ -17,6 +27,24 @@ public abstract class Task {
      * @throws ExecuteException 执行出错
      */
     public abstract void execute() throws ExecuteException;
+
+    /**
+     * 执行通知
+     * @param info 执行信息
+     */
+    public void executeInfo(String info) {
+        if (player != null) {
+            player.sendMessage(info);
+        }
+    }
+
+    /**
+     * 获取执行者名称
+     * @return 名称
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
 
     /**
      * 获取是否有后续的任务需要处理
