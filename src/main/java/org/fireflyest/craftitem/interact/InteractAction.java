a2c08a92c4bf4ef6b935d8eff692163ead505055
Fireflyest <fireflyest@qq.com>
2023-07-18 19:43:19 +0800

    交互物品雏形

--- src/main/java/org/fireflyest/craftitem/interact/InteractAction.java
+++ src/main/java/org/fireflyest/craftitem/interact/InteractAction.java
@@ -1,28 +1,35 @@
 package org.fireflyest.craftitem.interact;
 
 public class InteractAction {
-
-    // 类型 触发方式
-    public static final String NBT_INTERACT_TRIGGER = "craft-interact-trigger";
-    // 冷却
-    public static final String NBT_INTERACT_COOLDOWN = "craft-interact-cooldown";
-    // 耐久
-    public static final String NBT_INTERACT_DURABILITY = "craft-interact-durability";
-
-
-    public static final int TRIGGER_NONE = 0;
     
+    public static final String INTERACT_COOLDOWN = "craft-interact-cooldown";
+    public static final String INTERACT_DURABILITY = "craft-interact-durability";
+
+
+    // value actionName;durability;cooldown;num
     // 食物 吃完触发行为
-    public static final String TRIGGER_EAT = "eat";
+    public static final String TRIGGER_EAT = "craft-interact-eat";
     // 消耗品 右键触发行为
-    public static final String TRIGGER_USE = "use";
+    public static final String TRIGGER_USE = "craft-interact-use";
     // 武器 攻击到人后触发行为
-    public static final String TRIGGER_ATTACK = "attack";
+    public static final String TRIGGER_ATTACK = "craft-interact-attack";
+    public static final String TRIGGER_KILL = "craft-interact-kill";
     // 道具 右键生物或者方块触发行为
-    public static final String TRIGGER_ENTITY = "entity";
-    public static final String TRIGGER_BLOCK = "block";
+    public static final String TRIGGER_ENTITY = "craft-interact-entity";
+    public static final String TRIGGER_BLOCK = "craft-interact-block";
+    public static final String TRIGGER_PLACE = "craft-interact-place";
+    public static final String TRIGGER_BREAK = "craft-interact-break";
     // 切换到手上时触发
-    public static final String TRIGGER_HOLD = "hold";
+    public static final String TRIGGER_HOLD = "craft-interact-hold";
+
+
+
+    public static final String ACTION_CUSTOM = "custom";
+    public static final String ACTION_CUSTOM_DISPOSABLE = "custom-disposable";
+    public static final String ACTION_POTION = "potion";
+    public static final String ACTION_COMMAND = "command";
+    public static final String ACTION_CONSOLE = "console";
+    public static final String ACTION_CONSOLE_DISPOSABLE = "console-disposable";
 
 
 }
