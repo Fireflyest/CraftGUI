5629a36a98ebf1558f74935ec3b4521a3a6d2d80
Fireflyest <fireflyest@qq.com>
2024-05-21 10:33:14 +0800

    布尔类型参数提示修复

--- src/main/java/org/fireflyest/craftcommand/argument/StringArgs.java
+++ src/main/java/org/fireflyest/craftcommand/argument/StringArgs.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import org.bukkit.command.CommandSender;
 
 public class StringArgs implements Argument {
@@ -15,7 +17,7 @@ public class StringArgs implements Argument {
     }
 
     @Override
-    public List<String> tab(CommandSender sender, String arg) {
+    public List<String> tab(@Nonnull CommandSender sender, @Nonnull String arg) {
         List<String> ret = new ArrayList<>();
         for (String string : Arrays.asList(strings)) {
             if (string.startsWith(arg)) {
