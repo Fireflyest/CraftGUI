1a5ba042699ba15bc597db1eff564a70375ab0c0
Fireflyest <fireflyest@qq.com>
2024-05-25 19:38:50 +0800

    文本工具类

--- src/main/java/org/fireflyest/util/StringUtils.java
+++ src/main/java/org/fireflyest/util/StringUtils.java
@@ -1,6 +1,16 @@
 package org.fireflyest.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bukkit.util.NumberConversions;
+import org.fireflyest.util.RegexUtils.VariableContainer;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 /**
  * 一些字符串操作
@@ -10,20 +20,62 @@ import javax.annotation.Nonnull;
  */
 public class StringUtils {
     
+	private static final Gson gson = new Gson();
+
     private StringUtils() {
+    }
+
+    /**
+     * 格式化文本
+     * @param text 文本
+     * @param vars 变量
+     * @return 文本
+     */
+    public static String format(String text, String... vars) {
+        if (text == null) {
+            return null;
+        }
+        return RegexUtils.varReplace(RegexUtils.FORMAT_PATTERN, text, new VariableContainer() {
+            int index = 0;
+            @Override
+            public String getVar(String key) {
+                if ("".equals(key)) return vars[index++];
+                return vars[NumberConversions.toInt(key)];
+            }
+        });
     }
 
     /**
      * TEST_TEST转换为TestTest
      * @param str 文本
-     * @return 拼接
+     * @return 大驼峰
      */
     public static String toLowerCase(@Nonnull String str) {
         StringBuilder sb = new StringBuilder();
         for (String word : str.split("_")) {
-            sb.append(word.charAt(0))
-                    .append(word.substring(1).toLowerCase());
+            sb.append(word.charAt(0)).append(word.substring(1).toLowerCase());
         }
+        return sb.toString();
+    }
+
+	/**
+	 * TestTest转换为TEST_TEST
+	 * @param str 文本
+	 * @return 大写下划线
+	 */
+	public static String toUpperCase(@Nonnull String str) {
+        StringBuilder sb = new StringBuilder();
+		// 第一个字符大写
+		sb.append(str.substring(0, 1).toUpperCase());
+		for (int i = 1; i < str.length(); i++) {
+			char aChar = str.charAt(i);
+		   	// 在大写字母前添加下划线
+		   	if (Character.isUpperCase(aChar) && !Character.isDigit(aChar)) {
+				sb.append("_");
+			}
+			// 其他字符直接转成大写
+		   sb.append(Character.toUpperCase(aChar));
+		}
         return sb.toString();
     }
 
@@ -35,6 +87,36 @@ public class StringUtils {
     public static String toFirstUpCase(@Nonnull String str) {
         return Character.toUpperCase(str.charAt(0)) + str.substring(1).toLowerCase();
     }
+
+	/**
+	 * 列表转换为文本
+	 * @param list 列表
+	 * @return 文本
+	 */
+	public static String toJsonString(@Nonnull Object obj) {
+		return gson.toJson(obj);
+	}
+
+	/**
+	 * 文本转列表
+	 * @param <T> 对象类型
+	 * @param string 文本
+	 * @return 列表
+	 */
+	public static <T> List<T> stringToList(@Nullable String string) {
+        return string == null ? new ArrayList<>() : gson.fromJson(string, new TypeToken<List<T>>() {}.getType());
+	}
+
+	/**
+	 * 文本转对象
+	 * @param <T> 对象类型
+	 * @param string 文本
+	 * @param type 对象的类
+	 * @return 对象
+	 */
+	public static <T> T stringToObj(@Nullable String string, Class<T> type) {
+        return string == null ? null : gson.fromJson(string, type);
+	}
 
     /**
 	 * 版本号比较
